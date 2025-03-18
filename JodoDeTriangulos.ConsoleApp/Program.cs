76a89da4b717752adc12502b7c8215c2000260a0
Philipi Sauer <philipisauer09@gmail.com>
2025-03-18 15:39:37 -0300

    funçao inicial bem simples

--- JodoDeTriangulos.ConsoleApp/Program.cs
+++ JodoDeTriangulos.ConsoleApp/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            while(true) 
+            {
+                Console.Clear();
+                Console.WriteLine("triangulos");
+
+                Console.Write("informe x");
+                int ladoX = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("informe y");
+                int ladoY = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("informe z");
+                int ladoZ = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("valores informados: ");
+                Console.WriteLine("lado X" + ladoX);
+                Console.WriteLine("lado Y" + ladoY);
+                Console.WriteLine("lado Z" + ladoZ);
+
+
+                Console.Write("deseja continuar? (s/n)");
+                string opcaoContinuar = Console.ReadLine()!.ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
+                
+                    
+                }
+            }
     }
 }
